5fdfc9a08d7d210401b8ab7a17b098e0ad20c2a3
sachin <kolisachint@gmail.com>
2018-07-23 08:17:34 +0000

    Updated Resources

---
+++ IMDB2018Scrapper/IMDB2018FetchData.py
@@ -0,0 +1,16 @@
+# IMPORTING REQUIRED LIBRARY
+
+import os
+import sys
+import requests
+import BeautifulSoup
+from requests import get
+
+
+url = 'http://www.imdb.com/search/title?release_date=2017&sort=num_votes,desc&page=1'
+response = get(url)
+print(response.text[:500])  
+
+
+
+
---
+++ Resources/Output/README.md
@@ -0,0 +1,2 @@
+# Temporary Output Files
+
