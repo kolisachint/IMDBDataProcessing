f1fcd3045cabcae4ef744d170d451638c18d5a45
sachin <kolisachint@gmail.com>
2018-07-23 08:42:42 +0000

    test

--- IMDB2018Scrapper/IMDB2018FetchData.py
+++ IMDB2018Scrapper/IMDB2018FetchData.py
@@ -19,50 +19,6 @@ print(type(movie_containers))
 print(len(movie_containers))
 
 
-# Lists to store the scraped data in
-names = []
-years = []
-imdb_ratings = []
-metascores = []
-votes = []
-<<<<<<< HEAD
-=======
-
-# Extract data from individual movie container
-for container in movie_containers:
-
-    # If the movie has Metascore, then extract:
-    if container.find('div', class_ = 'ratings-metascore') is not None:
-
-        # The name
-        name = container.h3.a.text
-        names.append(name)
-
-        # The year
-        year = container.h3.find('span', class_ = 'lister-item-year').text
-        years.append(year)
-
-        # The IMDB rating
-        imdb = float(container.strong.text)
-        imdb_ratings.append(imdb)
-
-        # The Metascore
-        m_score = container.find('span', class_ = 'metascore').text
-        metascores.append(int(m_score))
-
-        # The number of votes
-        vote = container.find('span', attrs = {'name':'nv'})['data-value']
-        votes.append(int(vote))
-
-
-test_df = pd.DataFrame({'movie': names,
-                       'year': years,
-                       'imdb': imdb_ratings,
-                       'metascore': metascores,
-                       'votes': votes})
-print(test_df.info())
-test_df
->>>>>>> refs/remotes/origin/master
 
 # Extract data from individual movie container
 for container in movie_containers:
