28d50220ef9fa89cbe031f5cff724e7212d63642
sachin <kolisachint@gmail.com>
2018-07-24 10:19:32 +0000

    movies.csv

--- IMDB2018Scrapper/IMDB2018FetchData.py
+++ IMDB2018Scrapper/IMDB2018FetchData.py
@@ -63,7 +63,9 @@ movie_ratings = movie_ratings[['movie', 'year', 'imdb', 'metascore', 'votes']]
 movie_ratings.head(10)
 
 
-movie_ratings.to_csv('../Resources/Output/movie_ratings.csv')
+
+
+movie_ratings.to_json('../Resources/Output/movie_ratings.json')
 
 
 
